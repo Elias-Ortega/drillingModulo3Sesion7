36442e11149d69668dfd7856d855e95b7e7dc7a3
Elias Ortega <eliasortega2011@gmail.com>
2023-05-04 12:02:17 -0400

    estructura basica, index.html script.js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+const vehiculo = {
+    marca: "Goenx",
+    modelo: "",
+    motor:
+    {
+        tipoMotor: "4 CILINDROS EN LÍNEA",
+        presion: "",
+        bloque: "ALEACIÓN/ALUMINIO",
+        desplazamiento: "1.99/1996",
+        caballosDeFuerza: "155@ 6500",
+        lineaRoja: 6700,
+        smartAssist: "",
+        inyeccion: "PUNTOS MÚLTIPLES",
+        encendidoRemoto: "",
+
+    }
+}
